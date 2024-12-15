7e382c101f1d1ea7c137e5c1e1d8378174a96a73
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2024-12-15 14:12:23 +0530

    Time: 7 ms (51.02%) | Memory: 44.8 MB (20.07%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,39 +1,34 @@
 class Solution {
-    private String s;
-    private List<String> wordDict;
-    private int[] memo;
-
-    private boolean dp(int i) {
-        if (i < 0) return true;
-
-        if (memo[i] != -1) {
-            return memo[i] == 1;
-        }
-
-        for (String word : wordDict) {
-            // Handle out of bounds case
-            if (i - word.length() + 1 < 0) {
-                continue;
-            }
-
-            if (
-                s.substring(i - word.length() + 1, i + 1).equals(word) &&
-                dp(i - word.length())
-            ) {
-                memo[i] = 1;
-                return true;
-            }
-        }
-
-        memo[i] = 0;
-        return false;
-    }
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        this.s = s;
-        this.wordDict = wordDict;
-        this.memo = new int[s.length()];
-        Arrays.fill(this.memo, -1);
-        return dp(s.length() - 1);
+        int dp[][] = new int[s.length()][s.length()];
+        for(int i[]: dp)
+        {
+            Arrays.fill(i, -1);
+        }
+        return solve(0,0,s,new HashSet<>(wordDict), dp);
     }
+    private boolean solve(int start, int end, String s, Set<String> wd, int [][]dp)
+        {
+            if(dp[start][end] != -1)
+            {
+                return dp[start][end]==1 ? true: false;
+            }
+            if(end==s.length()-1)
+            {
+                if(wd.contains(s.substring(start,end+1))) return true;
+                return false;
+            }
+
+            if(wd.contains(s.substring(start,end+1)))
+            {
+                if(solve(end+1,end+1,s,wd,dp))
+                {
+                    dp[start][end]=1;
+                    return true;
+                }
+            }
+            boolean ans = solve(start, end+1, s,wd,dp);
+            dp[start][end] = ans?1:0;
+            return ans;
+        }
 }
