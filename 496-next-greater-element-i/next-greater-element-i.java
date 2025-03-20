432a526b29e67ce2f276d7c2f0914638f9a67617
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2025-03-20 11:00:36 +0530

    Time: 3 ms (90.35%) | Memory: 44.2 MB (37.90%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -6,17 +6,22 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         
         // Compute next greater elements for nums2
-        for (int num : nums2) {
-            while (!stack.isEmpty() && stack.peek() < num) {
-                map.put(stack.pop(), num);
+        for(int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
             }
-            stack.push(num);
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
         }
+
         int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             res[i] = map.getOrDefault(nums1[i], -1);
         }
-
         return res;
     }
 }
