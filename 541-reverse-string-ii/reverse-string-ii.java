33396b50aeb4b693a7455d3ad64bd4ddfea59066
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2025-03-25 14:27:26 +0530

    Time: 1 ms (81.01%) | Memory: 43.2 MB (93.88%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,24 +1,20 @@
 class Solution {
-    public void helper(StringBuilder sb, int start, int end)
-    {
-        while(start<end)
-        {
-            char ch = sb.charAt(start);
-            sb.setCharAt(start , sb.charAt(end));
-            sb.setCharAt(end ,ch);
+    public void helper(char chars[], int start, int end) {
+        while (start < end) { // Swap characters until the segment is fully reversed
+            char ch = chars[start];
+            chars[start] = chars[end];
+            chars[end] = ch;
             start++;
             end--;
         }
     }
+
     public String reverseStr(String s, int k) {
-        StringBuilder sb = new StringBuilder(s);
-        int i = 0;
-        while(i<s.length())
-        {
-            int j = Math.min(i+k-1, s.length()-1);
-            helper(sb,i,j);
-            i += 2*k;
+        char chars[] = s.toCharArray();
+        for (int i = 0; i < s.length(); i += 2 * k) {
+            int j = Math.min(i + k - 1, s.length() - 1); // Find the correct end index
+            helper(chars, i, j);
         }
-        return sb.toString();
+        return new String(chars);
     }
 }
