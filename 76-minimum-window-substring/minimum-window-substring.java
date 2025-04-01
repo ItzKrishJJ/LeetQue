610ec890fc8f9de56d048d82e6a80e399b3c70d9
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2025-04-01 14:03:54 +0530

    Time: 4 ms (79.06%) | Memory: 44.6 MB (93.65%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,43 +1,30 @@
-import java.util.*;
-
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() > s.length()) return "";
-        Map<Character, Integer> tChars = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            tChars.put(c, tChars.getOrDefault(c, 0) + 1);
+        int[] hash = new int[256];
+        int l = 0, r = 0, cnt = 0, minLen = Integer.MAX_VALUE, sIndex = -1;
+        
+        for (int i = 0; i < t.length(); i++) {
+            hash[t.charAt(i)]++;
         }
         
-        int r = 0, l = 0, minLen = Integer.MAX_VALUE, minStart = 0, matched = 0;
-        Map<Character, Integer> sChars = new HashMap<>();
-        
         while (r < s.length()) {
-            char ch = s.charAt(r);
-            if (tChars.containsKey(ch)) {
-                sChars.put(ch, sChars.getOrDefault(ch, 0) + 1);
-                if (sChars.get(ch).equals(tChars.get(ch))) {
-                    matched++;
-                }
-            }
+            if (hash[s.charAt(r)] > 0) cnt++;
+            hash[s.charAt(r)]--;
             
-            while (matched == tChars.size()) {
+            while (cnt == t.length()) {
                 if (r - l + 1 < minLen) {
                     minLen = r - l + 1;
-                    minStart = l;
+                    sIndex = l;
                 }
                 
-                char leftChar = s.charAt(l);
-                if (tChars.containsKey(leftChar)) {
-                    sChars.put(leftChar, sChars.get(leftChar) - 1);
-                    if (sChars.get(leftChar) < tChars.get(leftChar)) {
-                        matched--;
-                    }
-                }
+                hash[s.charAt(l)]++;
+                if (hash[s.charAt(l)] > 0) cnt--;
                 l++;
             }
+            
             r++;
         }
         
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        return sIndex == -1 ? "" : s.substring(sIndex, sIndex + minLen);
     }
 }
