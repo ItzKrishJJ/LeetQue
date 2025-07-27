50b1c1e88361d65fd289ae9a463c3828d89c4bf5
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2025-07-27 11:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (21.11%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,38 +1,18 @@
 class Solution {
-
     public int countHillValley(int[] nums) {
-        int res = 0; // number of peaks and valleys
-        int n = nums.length;
-        for (int i = 1; i < n - 1; ++i) {
-            if (nums[i] == nums[i - 1]) {
-                // deduplication
-                continue;
-            }
-            int left = 0; // left side possibly unequal neighboring corresponding state
-            for (int j = i - 1; j >= 0; --j) {
-                if (nums[j] > nums[i]) {
-                    left = 1;
-                    break;
-                } else if (nums[j] < nums[i]) {
-                    left = -1;
-                    break;
+        int prev = 0;
+        int count = 0;
+        for(int i=1;i<nums.length-1;i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                if((nums[i]>nums[prev] && nums[i]>nums[i+1]) || (nums[i]<nums[prev] && nums[i]<nums[i+1]))
+                {
+                    count++;
                 }
-            }
-            int right = 0; // right side possibly unequal neighboring corresponding state
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[j] > nums[i]) {
-                    right = 1;
-                    break;
-                } else if (nums[j] < nums[i]) {
-                    right = -1;
-                    break;
-                }
-            }
-            if (left == right && left != 0) {
-                // at this time, index i is part of a peak or valley.
-                ++res;
+                prev = i;
             }
         }
-        return res;
+        return count;
     }
 }
