c736377577cadd66d6c2cbf395f234bfc7c5ff16
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2026-01-23 17:18:37 +0530

    Time: 8 ms (51.40%) | Memory: 20.2 MB (22.35%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n<=1:
+            return 0
+        
+        steps = 0
+        jump = 0
+        currjump = 0
+        for i in range(n):
+            currjump  = max(currjump , nums[i] + i)
+            if i == jump:
+                jump = currjump
+                steps+=1
+
+                if jump >= n-1:
+                    break
+
+
+
+
+        return steps
+
